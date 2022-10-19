75c8be82776b55c5a84296b0a926eacfc5686e74
Kinjal Vithalani <kinjal@techuz.com>
2022-10-19 11:49:03 +0530

    showing logs for socket messaging status

--- socket/base.socket.js
+++ socket/base.socket.js
@@ -1,5 +1,6 @@
 const users = new Map();
 const Message = Models.Message;
+const User = Models.User;
 
 module.exports = {
     name: 'socket',
@@ -8,24 +9,30 @@ module.exports = {
         const io = require('socket.io')(server.listener);
 
         io.on('connection', (socket) => {
-            socket.on('initialize', (userId) => {
-                socket.userId = userId;
-                const user = users.get(socket.id);
+            socket.on('initialize', async (userId) => {
+                let user = await User.findOne({ _id: userId });
+                socket.user = user;
+                user = users.get(socket.id);
                 if (!user) {
                     users.set(socket.id, socket);
                 }
-                console.log(`socket user ${socket.userId} is connected`);
+                console.log(`${socket.user.name} is connected`);
             })
 
             socket.on('disconnect', () => {
                 users.delete(socket.id)
-                console.log(`socket user ${socket.userId} is disconnected`);
+                console.log(`${socket.user.name} is disconnected`);
             })
 
             socket.on('message', async (data) => {
                 try {
-                    console.log('on message data received: ', data);
+                    //console.log('on message data received: ', data);
                     const { to, from, message } = JSON.parse(data);
+
+                    const userFrom = await User.findOne({ _id: from });
+                    const userTo = await User.findOne({ _id: to });
+
+                    console.table({ from: userFrom.name, to: userTo.name, message: message });
 
                     for (const [k, v] of users) {
                         // console.log("v.userId: ", v.userId);
